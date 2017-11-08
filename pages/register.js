0d5157221dbaabc4e4a4100310f5e0d09d6adbaa
Trezy <tre@trezy.com>
2017-11-08 15:15:52 -0600

    [#5] Add registration page

---
+++ pages/register.js
@@ -0,0 +1,122 @@
+// Module imports
+import React from 'react'
+
+
+
+
+
+// Component imports
+import Component from '../components/Component'
+import Page from '../components/Page'
+
+
+
+
+
+// Component constants
+const title = 'Home'
+
+
+
+
+
+class Register extends Component {
+
+  /***************************************************************************\
+    Private Methods
+  \***************************************************************************/
+
+  _handleChange (event) {
+    let {
+      name,
+      value,
+    } = event.target
+    let newState = {}
+
+    newState[name] = value
+
+    this.setState(newState)
+  }
+
+  _onSubmit (event) {
+    event.preventDefault()
+
+    console.log('Registering:', this.state)
+  }
+
+
+
+
+
+  /***************************************************************************\
+    Public Methods
+  \***************************************************************************/
+
+  constructor (props) {
+    super(props)
+
+    this._bindMethods([
+      '_handleChange',
+      '_onSubmit',
+    ])
+
+    this.state = {
+      email: '',
+      name: '',
+      password: '',
+    }
+  }
+
+  render () {
+    let {
+      email,
+      name,
+      password,
+    } = this.state
+
+    return (
+      <form onSubmit={this._onSubmit}>
+        <header>
+          <h1>Register</h1>
+        </header>
+
+        <label htmlFor="name">Name</label>
+
+        <input
+          id="name"
+          name="name"
+          onChange={this._handleChange}
+          type="name"
+          value={name} />
+
+        <label htmlFor="email">Email</label>
+
+        <input
+          id="email"
+          name="email"
+          onChange={this._handleChange}
+          type="email"
+          value={email} />
+
+        <label htmlFor="password">Password</label>
+
+        <input
+          id="password"
+          name="password"
+          onChange={this._handleChange}
+          type="password"
+          value={password} />
+
+        <menu type="toolbar">
+          <button>Register</button>
+        </menu>
+      </form>
+    )
+  }
+}
+
+
+
+
+
+export default Page(Register, title)
