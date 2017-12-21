344b698315be650d27e91fa3828d9bc06664ea92
Trezy <tre@trezy.com>
2017-12-21 16:32:44 -0600

    Add a script for seeding the local database

---
+++ scripts/seed-db.js
@@ -0,0 +1,87 @@
+const faker = require('faker')
+const fs = require('fs')
+const mkdirp = require('mkdirp')
+const path = require('path')
+
+
+
+
+
+const characters = []
+const classes = [
+  'Barbarian',
+  'Bard',
+  'Cleric',
+  'Druid',
+  'Fighter',
+  'Monk',
+  'Paladin',
+  'Ranger',
+  'Rogue',
+  'Sorcerer',
+  'Warlock',
+  'Wizard',
+]
+const races = [
+  'Dragonborn',
+  'Dwarf',
+  'Elf',
+  'Gnome',
+  'Half-Elf',
+  'Half-Orc',
+  'Halfling',
+  'Human',
+  'Tiefling',
+]
+const users = []
+
+
+
+
+
+// Generators
+const generateCharacter = userId => ({
+  class: classes[Math.floor(Math.random() * classes.length)],
+  id: faker.random.uuid(),
+  level: Math.ceil(Math.random() * 20),
+  name: faker.fake('{{name.firstName}} {{name.lastName}}'),
+  owner: userId,
+  race: races[Math.floor(Math.random() * races.length)],
+})
+
+const generateUser = () => ({
+  id: faker.random.uuid(),
+  name: faker.fake('{{name.firstName}} {{name.lastName}}'),
+})
+
+
+
+
+
+// Generate the entities
+let usersToGenerate = 20
+let charactersToGenerate = 100
+
+while (usersToGenerate-- > 0) {
+  users.push(generateUser())
+}
+
+while (charactersToGenerate-- > 0) {
+  characters.push(generateCharacter(users[Math.floor(Math.random() * users.length)].id))
+}
+
+
+
+
+
+// Write the entities to disk
+mkdirp.sync(path.resolve('data', 'users'))
+mkdirp.sync(path.resolve('data', 'characters'))
+
+for (const user of users) {
+  fs.writeFileSync(path.resolve('data', 'users', `${user.id}.json`), JSON.stringify(user), 'utf8')
+}
+
+for (const character of characters) {
+  fs.writeFileSync(path.resolve('data', 'characters', `${character.id}.json`), JSON.stringify(character), 'utf8')
+}
