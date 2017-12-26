8a029be1782fd66f22d3351d1c4822ab70baa462
Trezy <tre@trezy.com>
2017-12-26 11:39:12 -0600

    Add character building wizard with race and class steps

---
+++ components/Chooser.js
@@ -0,0 +1,54 @@
+// Module imports
+import Component from './Component'
+
+
+
+
+
+export default class extends Component {
+  /***************************************************************************\
+    Public Methods
+  \***************************************************************************/
+
+  _handleClick (option) {
+    if (this.props.onChange && (this.props.value !== option)) {
+      this.props.onChange(option)
+    }
+  }
+
+
+
+
+
+  /***************************************************************************\
+    Public Methods
+  \***************************************************************************/
+
+  componentWillUnmount () {
+    console.log('componentWillUnmount')
+  }
+
+  isValidated () {
+    return !!this.props.value
+  }
+
+  render () {
+    const {
+      options,
+      value,
+    } = this.props
+
+    return (
+      <div>
+        {options.map(option => (
+          <button
+            className={(value === option) ? 'active' : null}
+            key={option}
+            onClick={() => this._handleClick(option)}>
+            {option}
+          </button>
+        ))}
+      </div>
+    )
+  }
+}
--- pages/character-builder.js
+++ pages/character-builder.js
@@ -1,14 +1,17 @@
 // Module imports
 import React from 'react'
+import Stepzilla from 'react-stepzilla'
 
 
 
 
 
 // Component imports
+// import ClassChooser from '../components/ClassChooser'
+import Chooser from '../components/Chooser'
 import Component from '../components/Component'
 import Page from '../components/Page'
-import SimpleDropdown from '../components/SimpleDropdown'
+// import RaceChooser from '../components/RaceChooser'
 
 
 
@@ -38,6 +41,17 @@ class CharacterBuilder extends Component {
     this.setState(newState)
   }
 
+  _handleStepChange (step) {
+    console.log(step, this)
+  }
+
+  _handleSubmit () {
+    console.group('Creating new character')
+    console.log('Class:', this.state.class)
+    console.log('Race:', this.state.race)
+    console.groupEnd()
+  }
+
 
 
 
@@ -46,186 +60,52 @@ class CharacterBuilder extends Component {
     Public Methods
   \***************************************************************************/
 
+  componentDidUpdate () {
+    console.group('Creating new character')
+    console.log('Class:', this.state.class)
+    console.log('Race:', this.state.race)
+    console.groupEnd()
+  }
+
   constructor (props) {
     super(props)
 
     this._bindMethods(['_handleChange'])
 
     this.state = {
-      background: '',
-      charisma: '',
-      constitution: '',
-      dexterity: '',
-      ethic: '',
-      experience: '',
-      intelligence: '',
-      moral: '',
-      name: '',
-      oClass: '',
-      race: '',
-      strength: '',
-      wisdom: '',
+      class: null,
+      race: null,
     }
   }
 
   render () {
     const {
-      background,
-      charisma,
-      constitution,
-      dexterity,
-      ethic,
-      experience,
-      intelligence,
-      moral,
-      name,
-      oClass,
-      race,
-      strength,
-      wisdom,
-    } = this.state
-    const backgrounds = [
-      'acolyte',
-      'pirate',
-      'Chump',
-      'shmoe',
-      'greg',
-      'podcaster',
-    ]
-    const classes = [
-      'barb',
-      'bard',
-      'ranger',
-      'quigley',
-      'joe',
-      'json',
-    ]
-    const ethics = [
-      'chaotic',
-      'neutral',
-      'lawful',
-    ]
-    const morals = [
-      'good',
-      'neutral',
-      'evil',
-    ]
-    const races = [
-      'dwarf',
-      'other dwarf',
-      'high elf',
-      'wood elf',
-      'dark elf',
-    ]
+      ruleset,
+    } = this.props
 
     return (
-      <form>
-        <header>
-          <h2>General Shit</h2>
-        </header>
-
-        <input
-          name="name"
-          onChange={this._handleChange}
-          placeholder="Character Name"
-          type="text"
-          value={name} />
-
-        <SimpleDropdown
-          name="race"
-          onChange={this._handleChange}
-          options={races}
-          value={race} />
-
-        <SimpleDropdown
-          name="oClass"
-          onChange={this._handleChange}
-          options={classes}
-          value={oClass} />
-
-        <SimpleDropdown
-          name="background"
-          onChange={this._handleChange}
-          options={backgrounds}
-          value={background} />
-
-        <SimpleDropdown
-          name="ethic"
-          onChange={this._handleChange}
-          options={ethics}
-          value={ethic} />
-
-        <SimpleDropdown
-          name="moral"
-          onChange={this._handleChange}
-          options={morals}
-          value={moral} />
-
-        <input
-          name="experience"
-          onChange={this._handleChange}
-          placeholder="XP"
-          type="number"
-          value={experience} />
-
-        <header>
-          <h2>Ability Scores</h2>
-        </header>
-
-        <input
-          name="strength"
-          onChange={this._handleChange}
-          placeholder="Strength"
-          type="number" />
-
-        <input
-          name="constitution"
-          onChange={this._handleChange}
-          placeholder="Constitution"
-          type="number" />
-
-        <input
-          name="dexterity"
-          onChange={this._handleChange}
-          placeholder="Dexterity"
-          type="number" />
-
-        <input
-          name="intelligence"
-          onChange={this._handleChange}
-          placeholder="Intelligence"
-          type="number" />
-
-        <input
-          name="wisdom"
-          onChange={this._handleChange}
-          placeholder="Wisdom"
-          type="number" />
-
-        <input
-          name="charisma"
-          onChange={this._handleChange}
-          placeholder="Charisma"
-          type="number" />
-
-        <br />
-
-        name: {name}<br />
-        experience: {experience}<br />
-        background: {background}<br />
-        charisma: {charisma}<br />
-        constitution: {constitution}<br />
-        dexterity: {dexterity}<br />
-        ethic: {ethic}<br />
-        experience: {experience}<br />
-        intelligence: {intelligence}<br />
-        moral: {moral}<br />
-        name: {name}<br />
-        oClass: {oClass}<br />
-        race: {race}<br />
-        strength: {strength}<br />
-        wisdom: {wisdom}<br />
-      </form>
+      <Stepzilla
+        onStepChange={step => console.log('onStepChange', step)}
+        steps={[
+          {
+            name: 'Choose your race...',
+            component: <Chooser
+              onChange={(value) => this.setState({ race: value })}
+              options={ruleset['player-characters'].races}
+              value={this.state.race} />,
+          },
+          {
+            name: 'Choose your class...',
+            component: <Chooser
+              onChange={(value) => this.setState({ class: value })}
+              options={ruleset['player-characters'].classes}
+              value={this.state.class} />,
+          },
+          {
+            name: 'Determine your ability scores...',
+            component: <div />,
+          },
+        ]} />
     )
   }
 }
@@ -234,4 +114,15 @@ class CharacterBuilder extends Component {
 
 
 
-export default Page(CharacterBuilder, title)
+const mapStateToDispatch = ['createCharacter']
+
+const mapStateToProps = state => ({ ruleset: state.rulesets['dnd-5e'] })
+
+
+
+
+
+export default Page(CharacterBuilder, title, {
+  mapStateToDispatch,
+  mapStateToProps,
+})
