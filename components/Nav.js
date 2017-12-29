fd4883733b37083bdfefde71d58bd424c05301e5
Trezy <tre@trezy.com>
2017-12-29 15:16:00 -0600

    Remove all non-functional links

--- components/Nav.js
+++ components/Nav.js
@@ -18,10 +18,10 @@ const navItems = [
   {
     title: 'Player Tools',
     subnav: [
-      {
-        href: '/groups/manage',
-        title: 'Groups',
-      },
+      // {
+      //   href: '/groups/manage',
+      //   title: 'Groups',
+      // },
 
       {
         href: '/my/characters',
@@ -30,45 +30,45 @@ const navItems = [
     ],
   },
 
-  {
-    title: 'GM Tools',
-    subnav: [
-      {
-        href: '/groups/manage',
-        title: 'Groups',
-      },
+  // {
+  //   title: 'GM Tools',
+  //   subnav: [
+  //     {
+  //       href: '/groups/manage',
+  //       title: 'Groups',
+  //     },
 
-      {
-        href: '/gm/dungeons',
-        title: 'Dungeons',
-      },
+  //     {
+  //       href: '/gm/dungeons',
+  //       title: 'Dungeons',
+  //     },
 
-      {
-        href: '/gm/encounters',
-        title: 'Encounters',
-      },
+  //     {
+  //       href: '/gm/encounters',
+  //       title: 'Encounters',
+  //     },
 
-      {
-        href: '/gm/monsters',
-        title: 'Monsters',
-      },
+  //     {
+  //       href: '/gm/monsters',
+  //       title: 'Monsters',
+  //     },
 
-      {
-        href: '/gm/npcs',
-        title: 'NPCs',
-      },
+  //     {
+  //       href: '/gm/npcs',
+  //       title: 'NPCs',
+  //     },
 
-      {
-        href: '/gm/treasure',
-        title: 'Treasure',
-      },
-    ],
-  },
+  //     {
+  //       href: '/gm/treasure',
+  //       title: 'Treasure',
+  //     },
+  //   ],
+  // },
 
-  {
-    href: '/login',
-    title: 'Login/Register',
-  },
+  // {
+  //   href: '/login',
+  //   title: 'Login/Register',
+  // },
 ]
 
 
