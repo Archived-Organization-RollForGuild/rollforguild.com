88f5114ba38fdfa89f4a27a6d2f0d3175d26677f
Trezy <tre@trezy.com>
2018-01-05 15:22:10 -0600

    feat(chooser): Add list style and validation
    * Add default props and prop validation.
    * Add list style. This allows `Chooser`s to display as vertical columns
      instead of grids
    
    for #14

--- components/Chooser.js
+++ components/Chooser.js
@@ -1,30 +1,75 @@
 // Module imports
+import PropTypes from 'prop-types'
+
+
+
+
+// Module imports
 import Component from './Component'
 
 
 
 
 
-export default class extends Component {
+class Chooser extends Component {
   /***************************************************************************\
-    Public Methods
+    Private Methods
   \***************************************************************************/
 
   _handleClick (option) {
     const {
       onChange,
+      renderButton,
       returnValue,
       value,
     } = this.props
-    const optionValue = returnValue ? returnValue(option) : option
+    const optionValue = returnValue(option)
 
-    if (onChange) {
+    if (renderButton && onChange) {
       if (value !== optionValue) {
         onChange(optionValue)
       } else {
         onChange(null)
       }
     }
+  }
+
+  _renderOption (option) {
+    const {
+      renderButton,
+      renderOption,
+      value,
+    } = this.props
+    const renderedOption = renderOption(option)
+    const classes = []
+
+    if (value) {
+      if (value === option.value) {
+        classes.push('active')
+      } else {
+        classes.push('inactive')
+      }
+    }
+
+    if (renderButton) {
+      return (
+        <li key={renderedOption}>
+          <button
+            className={classes.join(' ')}
+            onClick={() => this._handleClick(option)}>
+            {renderedOption}
+          </button>
+        </li>
+      )
+    }
+
+    return (
+      <li
+        className={classes.join(' ')}
+        key={renderedOption}>
+        {renderedOption}
+      </li>
+    )
   }
 
 
@@ -35,12 +80,16 @@ export default class extends Component {
     Public Methods
   \***************************************************************************/
 
+  constructor (props) {
+    super(props)
+
+    this._bindMethods(['_renderOption'])
+  }
+
   render () {
     const {
       className,
       options,
-      renderOption,
-      value,
     } = this.props
 
     let renderedOptions = options
@@ -60,28 +109,7 @@ export default class extends Component {
       return option
     })
 
-    renderedOptions = renderedOptions.map(option => {
-      const renderedOption = renderOption ? renderOption(option) : option
-      const classes = []
-
-      if (value) {
-        if (value === option.value) {
-          classes.push('active')
-        } else {
-          classes.push('inactive')
-        }
-      }
-
-      return (
-        <li key={renderedOption}>
-          <button
-            className={classes.join(' ')}
-            onClick={() => this._handleClick(option)}>
-            {renderedOption}
-          </button>
-        </li>
-      )
-    })
+    renderedOptions = renderedOptions.map(this._renderOption)
 
     return (
       <ul className={['chooser', className || null].join(' ')}>
@@ -90,3 +118,28 @@ export default class extends Component {
     )
   }
 }
+
+Chooser.defaultProps = {
+  className: null,
+  onChange: null,
+  renderButton: true,
+  renderOption: option => option,
+  returnValue: option => option,
+  value: null,
+}
+
+Chooser.propTypes = {
+  className: PropTypes.string,
+  onChange: PropTypes.func,
+  options: PropTypes.array.isRequired,
+  renderButton: PropTypes.bool,
+  renderOption: PropTypes.func,
+  returnValue: PropTypes.func,
+  value: PropTypes.any,
+}
+
+
+
+
+
+export default Chooser
