d809c3a2548ab5d05fc08af0f6b77da1ceac96fc
Trezy <tre@trezy.com>
2018-01-14 09:08:44 -0600

    fix(analytics): Update GTM to use correct environment variable
    Close #44

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ import Document, { Head, Main, NextScript } from 'next/document'
 
 
 const fonts = ['Lora', 'Montserrat:400,700']
-const gatmId = preval`module.exports = process.env.GA_TAG_MANAGER_ID`
+const gatmId = preval`module.exports = process.env.RFG_GA_TAG_MANAGER_ID`
 
 
 
