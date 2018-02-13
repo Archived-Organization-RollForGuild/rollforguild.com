ca23b4ab187efb197912fcb9dab0dcac3186de05
Trezy <tre@trezy.com>
2018-02-12 21:57:06 -0600

    feat(group-search): Add group search page

---
+++ pages/groups/search.js
@@ -0,0 +1,23 @@
+// Component imports
+import Page from '../../components/Page'
+
+
+
+
+
+// Component constants
+const title = 'Search Groups'
+
+
+
+
+
+const SearchGroups = () => (
+  <h1>Search Groups</h1>
+)
+
+
+
+
+
+export default Page(SearchGroups, title)
