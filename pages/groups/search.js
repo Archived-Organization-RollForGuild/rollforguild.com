7e0208d5ea1917ba3851079d1eed33c9198f0f9b
Trezy <tre@trezy.com>
2018-02-19 17:55:01 -0600

    feat(group-search): Add option to use current location

--- pages/groups/search.js
+++ pages/groups/search.js
@@ -12,12 +12,14 @@ import Dropdown from '../../components/Dropdown'
 import GroupCard from '../../components/GroupCard'
 import Page from '../../components/Page'
 import Pagination from '../../components/Pagination'
+import convertObjectToQueryParams from '../../helpers/convertObjectToQueryParams'
 
 
 
 
 
 // Component constants
+const gmapsAPIKey = preval`module.exports = process.env.RFG_GOOGLE_MAPS_API_KEY`
 const title = 'Search Groups'
 
 
@@ -29,8 +31,47 @@ class GroupSearch extends Component {
     Private Methods
   \***************************************************************************/
 
-  _handleAddressChange ({ geometry }) {
-    this.setState({ location: geometry.location }, () => this._search())
+  _handleAddressChange (location) {
+    this.setState({
+      location: {
+        ...location.geometry.location,
+        address: location.formatted_address,
+      },
+    }, () => this._search())
+  }
+
+  async _handleGeolocationUpdate (position) {
+    const {
+      latitude,
+      longitude,
+    } = position.coords
+    const queryParams = {
+      key: gmapsAPIKey,
+      latlng: `${latitude},${longitude}`,
+      result_type: 'street_address',
+    }
+
+    let response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json${convertObjectToQueryParams(queryParams)}`)
+
+    response = await response.json()
+
+    this.setState({
+      location: {
+        address: response.results[0].formatted_address,
+        lat: latitude,
+        lng: longitude,
+      },
+      waitingForLocation: false,
+    }, () => this._search())
+  }
+
+  _handleGeolocationError () {
+    navigator.geolocation.clearWatch(this.wpid)
+
+    this.setState({
+      waitingForLocation: false,
+      watchingLocation: false,
+    })
   }
 
   _handleSearchDistanceChange (distance) {
@@ -108,6 +149,25 @@ class GroupSearch extends Component {
     setTimeout(() => this.setState(newState), 500)
   }
 
+  async _toggleUseCurrentLocation ({ target }) {
+    this.setState({
+      location: null,
+      useCurrentLocation: target.checked,
+      waitingForLocation: target.checked,
+      watchingLocation: target.checked,
+    })
+
+    if (target.checked) {
+      this.wpid = navigator.geolocation.watchPosition(this._handleGeolocationUpdate, this._handleGeolocationError, {
+        enableHighAccuracy: true,
+        maximumAge: 30000,
+        timeout: 27000,
+      })
+    } else {
+      navigator.geolocation.clearWatch(this.wpid)
+    }
+  }
+
 
 
 
@@ -116,14 +176,21 @@ class GroupSearch extends Component {
     Public Methods
   \***************************************************************************/
 
+  componentWillUnmount () {
+    navigator.geolocation.clearWatch(this.wpid)
+  }
+
   constructor (props) {
     super(props)
 
     this._bindMethods([
       '_handleAddressChange',
+      '_handleGeolocationError',
+      '_handleGeolocationUpdate',
       '_handleSearchDistanceChange',
       '_incrementSearchDistance',
       '_search',
+      '_toggleUseCurrentLocation',
     ])
 
     this._debounceMethods(['_search'])
@@ -138,6 +205,9 @@ class GroupSearch extends Component {
       },
       searchDistance: 5,
       searching: false,
+      useCurrentLocation: false,
+      waitingForLocation: false,
+      watchingLocation: false,
     }
   }
 
@@ -145,9 +215,13 @@ class GroupSearch extends Component {
     const {
       firstSearchInitiated,
       groups,
+      location,
       pagination,
       searchDistance,
       searching,
+      useCurrentLocation,
+      waitingForLocation,
+      watchingLocation,
     } = this.state
 
     return (
@@ -158,9 +232,16 @@ class GroupSearch extends Component {
 
         <fieldset>
           <div className="input-group">
-            <i className="fas fa-fw fa-search" />
+            <label>
+              <i className="fas fa-fw fa-search" />
+            </label>
 
-            <AddressInput onChange={this._handleAddressChange} />
+            <AddressInput
+              onChange={this._handleAddressChange}
+              disabled={waitingForLocation}
+              placeholder={waitingForLocation ? 'Loading...' : 'Enter an address...'}
+              readOnly={watchingLocation}
+              value={location ? location.address : ''} />
 
             <Dropdown
               className="squishable"
@@ -170,7 +251,32 @@ class GroupSearch extends Component {
               value={searchDistance} />
           </div>
 
-          <small>Use your location to search for nearby groups</small>
+          <footer>
+            <p>Use your location to search for nearby groups</p>
+
+            <div className="options">
+              Options:
+
+              <ul>
+                <li>
+                  <input
+                    id="use-current-location"
+                    hidden
+                    onChange={this._toggleUseCurrentLocation}
+                    type="checkbox"
+                    value={useCurrentLocation} />
+
+                  <label
+                    className="binary button inline"
+                    data-on={useCurrentLocation}
+                    htmlFor="use-current-location">
+                    <i className="fas fa-fw fa-map-marker" />
+                    Use my current location
+                  </label>
+                </li>
+              </ul>
+            </div>
+          </footer>
         </fieldset>
 
         {(!searching && !!groups.length) && this._renderGroups()}
