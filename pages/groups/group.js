5d0c9f41081660bde10578fc75c49dba56dfde07
Trezy <tre@trezy.com>
2018-02-19 21:42:34 -0600

    feat(group-profile): Only show map to members

--- pages/groups/group.js
+++ pages/groups/group.js
@@ -8,7 +8,6 @@ import React from 'react'
 
 // Component imports
 import Component from '../../components/Component'
-// import Map from '../../components/Map'
 import Page from '../../components/Page'
 import StaticMap from '../../components/StaticMap'
 
@@ -17,13 +16,13 @@ import StaticMap from '../../components/StaticMap'
 
 
 // Component constants
-const title = 'Manage Group'
+const title = 'Group'
 
 
 
 
 
-class ManageGroup extends Component {
+class GroupProfile extends Component {
   /***************************************************************************\
     Public Methods
   \***************************************************************************/
@@ -31,112 +30,125 @@ class ManageGroup extends Component {
   async componentDidMount () {
     const { getGroup } = this.props
     const { id } = this.props.query
-    let { group } = this.props
+    const { group } = this.props
 
     if (!group) {
-      const { payload } = await getGroup(id)
-
-      group = payload.data
+      await getGroup(id)
     }
 
-    this.setState({
-      group,
-      loaded: true,
-    })
+    this.setState({ loaded: true })
   }
 
   constructor (props) {
     super(props)
 
-    this.state = {
-      group: null,
-      loaded: false,
-    }
+    this.state = { loaded: false }
   }
 
   render () {
+    const { loaded } = this.state
     const {
-      loaded,
+      members,
       group,
-    } = this.state
+    } = this.props
+
+    if (!group && !loaded) {
+      return (
+        <React.Fragment>
+          <header>
+            <h1>Group</h1>
+          </header>
+
+          <p>Loading...</p>
+        </React.Fragment>
+      )
+    }
+
+    if (!group) {
+      return (
+        <React.Fragment>
+          <header>
+            <h1>Group</h1>
+          </header>
+
+          <p>No group with that ID was found.</p>
+        </React.Fragment>
+      )
+    }
+
+    const {
+      description,
+      games,
+      geo,
+      name,
+    } = group.attributes
+    const memberStatus = group.attributes.member_status // Because camelCase
 
     return (
       <React.Fragment>
         <header>
-          <h1>Manage {group ? group.attributes.name : 'Group'}</h1>
+          <h1>{name}</h1>
         </header>
 
-        {!loaded && (
-          <p>Loading...</p>
+        <div className="games">
+          <h2>Games</h2>
+          <ul className="comma-separated inline">
+            {games.map(game => (
+              <li key={game}>{game}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="description">
+          <h2>Description</h2>
+          <p>{description || 'No description.'}</p>
+        </div>
+
+        {(!!memberStatus && (memberStatus !== 'pending')) && (
+          <div className="location">
+            <h2>Location</h2>
+
+            <StaticMap
+              location={geo}
+              markers={[{ ...geo }]} />
+          </div>
         )}
 
-        {(loaded && !group) && (
-          <p>No group with that ID was found.</p>
-        )}
+        {members && (
+          <div className="players">
+            <header>Members:</header>
 
-        {(loaded && group) && (
-          <React.Fragment>
-            <div className="games">
-              <h2>Games</h2>
-              <ul className="comma-separated inline">
-                {group.attributes.games.map(game => (
-                  <li key={game}>{game}</li>
-                ))}
-              </ul>
-            </div>
+            <ul>
+              {members.map(({ attributes, id }) => (
+                <li key={id}>
+                  <img
+                    alt={`Avatar for ${attributes.username}`}
+                    className="avatar"
+                    src={`//api.adorable.io/avatars/50/${id}`} />
 
-            <div className="description">
-              <h2>Description</h2>
-              <p>{group.attributes.description || 'No description.'}</p>
-            </div>
+                  <header>
+                    {attributes.username}
+                  </header>
 
-            <div className="location">
-              <h2>Location</h2>
+                  <menu
+                    className="compact"
+                    type="toolbar">
+                    <div className="primary">
+                      <a
+                        className="button small success"
+                        href={`mailto:${attributes.email}`}>
+                        Message
+                      </a>
 
-              <StaticMap
-                location={group.attributes.geo}
-                markers={[
-                  { ...group.attributes.geo },
-                ]} />
-            </div>
-
-            {group.relationships && (
-              <div className="players">
-                <header>Members:</header>
-
-                <ul>
-                  {group.relationships.group_members.map(({ attributes, id }) => (
-                    <li key={id}>
-                      <img
-                        alt={`Avatar for ${attributes.username}`}
-                        className="avatar"
-                        src={`//api.adorable.io/avatars/50/${id}`} />
-
-                      <header>
-                        {attributes.username}
-                      </header>
-
-                      <menu
-                        className="compact"
-                        type="toolbar">
-                        <div className="primary">
-                          <a
-                            className="button small success"
-                            href={`mailto:${attributes.email}`}>
-                            Message
-                          </a>
-
-                          <button className="danger small">
-                            Remove
-                          </button>
-                        </div>
-                      </menu>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-          </React.Fragment>
+                      <button className="danger small">
+                        Remove
+                      </button>
+                    </div>
+                  </menu>
+                </li>
+              ))}
+            </ul>
+          </div>
         )}
       </React.Fragment>
     )
@@ -149,13 +161,25 @@ class ManageGroup extends Component {
 
 const mapDispatchToProps = ['getGroup']
 
-const mapStateToProps = (state, ownProps) => ({ group: state.groups[ownProps.query.id] || null })
+const mapStateToProps = (state, ownProps) => {
+  const group = state.groups[ownProps.query.id] || null
+  let members = null
+
+  if (group && group.attributes.member_status && group.attributes.member_status !== 'pending') {
+    members = group.relationships.group_members.data.map(member => state.users[member.id])
+  }
+
+  return {
+    group,
+    members,
+  }
+}
 
 
 
 
 
-export default Page(ManageGroup, title, {
+export default Page(GroupProfile, title, {
   mapDispatchToProps,
   mapStateToProps,
 })
