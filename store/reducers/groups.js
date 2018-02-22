adb6c402ef0b679ca0c35ca63159fc6c74455f9f
Trezy <tre@trezy.com>
2018-02-22 10:56:06 -0600

    feat(user-profile): Capture groups from user profile

--- store/reducers/groups.js
+++ store/reducers/groups.js
@@ -15,10 +15,26 @@ export default function (state = initialState.groups, action) {
   switch (type) {
     case actionTypes.CREATE_GROUP:
     case actionTypes.GET_GROUP:
+    case actionTypes.GET_USER:
       if (status === 'success') {
+        let newGroups = parseJSONAPIResponseForEntityType(payload, 'groups', true)
+
+        newGroups = Object.values(newGroups).reduce((accumulator, group) => {
+          const oldGroup = state[group.id]
+          const memberStatus = group.member_status || (oldGroup ? oldGroup.member_status : null)
+
+          return {
+            ...accumulator,
+            [group.id]: {
+              ...group,
+              member_status: memberStatus,
+            },
+          }
+        }, {})
+
         return {
           ...state,
-          ...parseJSONAPIResponseForEntityType(payload, 'groups', true),
+          ...newGroups,
         }
       }
       return { ...state }
