4ea7ee289de37f6cae46824cd18f46b642e3cbea
Cameron Welter <camjwelt@gmail.com>
2018-03-08 18:48:54 +0000

    refactor(PasswordInput): fix everything about PasswordInput so it works

--- components/PasswordInput.js
+++ components/PasswordInput.js
@@ -8,33 +8,34 @@ import zxcvbn from 'zxcvbn'
 
 
 // Component imports
-import Component from './Component'
 import ValidatedInput from './ValidatedInput'
 
 
 
 
 
-class PasswordInput extends Component {
+class PasswordInput extends ValidatedInput {
   /***************************************************************************\
     Private Methods
   \***************************************************************************/
 
-  _getMessages(value) {
+  _validate() {
     const {
       showWarnings,
       showSuggestions,
     } = this.props
 
     const messages = []
-    const passwordEvaluation = zxcvbn(value)
+    const passwordEvaluation = zxcvbn(this._el.value)
+
+    let isValid = true
 
     if (showWarnings && passwordEvaluation.feedback.warning) {
       messages.push({
         icon: 'exclamation-triangle',
         message: passwordEvaluation.feedback.warning,
-        priority: 99,
       })
+      isValid = false
     }
 
     if (showSuggestions && passwordEvaluation.feedback.suggestions.length) {
@@ -42,22 +43,23 @@ class PasswordInput extends Component {
         messages.push({
           icon: 'exclamation-circle',
           message: suggestion,
-          priority: -1,
         })
       }
+      isValid = false
     }
 
     this.setState({
       passwordStrength: passwordEvaluation.score,
+      isValid,
     })
 
-    return messages
+    super._validate(messages)
   }
 
   _handleShowPasswordClick (event) {
     event.preventDefault()
     this.setState({ showPassword: !this.state.showPassword })
-    this._inputEl.focus()
+    this._el.focus()
   }
 
   /***************************************************************************\
@@ -67,53 +69,82 @@ class PasswordInput extends Component {
     super(props)
 
     this._bindMethods([
-      '_getMessages',
       '_handleShowPasswordClick',
     ])
 
     this.state = {
+      ...this.state,
       showPassword: false,
       passwordStrength: 0,
+      isValid: true,
     }
   }
-
   render() {
     const {
       showPassword,
-      showStrength,
       passwordStrength,
+      messages,
+      isValid,
     } = this.state
 
+    const {
+      showStrength,
+    } = this.props
+
+    const classNames = [
+      'validated-input',
+      'password-input',
+      (this.props.className || ''),
+    ]
+
+    const inputProps = { ...this.props }
+    delete inputProps.onValidate
+    delete inputProps.className
+    delete inputProps.showStrength
+    delete inputProps.showWarnings
+    delete inputProps.showSuggestions
+
+    if (!isValid) {
+      inputProps.pattern = '$.^'
+    }
+
     return (
-      <div className="password-input">
-        <ValidatedInput
-          {...this.props}
-          messages={this._getMessages}
-          inputRef={inputEl => this._inputEl = inputEl}
-          type={showPassword ? 'text' : 'password'}>
+      <div className={classNames.join(' ')}>
+        <input
+          {...inputProps}
+          ref={_el => this._el = _el}
+          type={showPassword ? 'text' : 'password'} />
 
-          <button
-            className="show-password"
-            onClick={this._handleShowPasswordClick}>
-            <FontAwesomeIcon icon={showPassword ? 'eye-slash' : 'eye'} fixedWidth />
-          </button>
+        <button
+          className="show-password"
+          onClick={this._handleShowPasswordClick}>
+          <FontAwesomeIcon icon={showPassword ? 'eye-slash' : 'eye'} fixedWidth />
+        </button>
 
-          {showStrength && (
-            <meter
-              className="password-strength-meter"
-              low="2"
-              high="3"
-              max="4"
-              optimum="4"
-              value={passwordStrength} />
-          )}
+        {showStrength && (
+          <meter
+            className="strength-meter"
+            low="2"
+            high="3"
+            max="4"
+            optimum="4"
+            value={passwordStrength} />
+        )}
 
-        </ValidatedInput>
+        <FontAwesomeIcon className="validity-indicator" icon="exclamation-triangle" fixedWidth />
+
+        <ul className="messages">
+          {messages.map(({ icon, message }) => (
+            <li key={message}>
+              <FontAwesomeIcon icon={icon} fixedWidth />
+              {message}
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }
 }
-
 
 
 
