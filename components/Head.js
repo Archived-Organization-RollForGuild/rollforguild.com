fb74c5739657e3d17d3b1e228132ba1818d01030
Trezy <tre@trezy.com>
2018-03-16 09:25:52 -0500

    refactor(*): Remove Twitter and Facebook snippets
    I've moved them into Tag Manager

--- components/Head.js
+++ components/Head.js
@@ -1,3 +1,4 @@
+// Module imports
 import NextHead from 'next/head'
 import NProgress from 'nprogress'
 import React from 'react'
@@ -6,6 +7,7 @@ import React from 'react'
 
 
 
+// Component imports
 import { Router } from '../routes'
 import appStylesheet from '../scss/app.scss'
 import libStylesheet from '../scss/lib.scss'
--- pages/_document.js
+++ pages/_document.js
@@ -54,50 +54,6 @@ export default class extends Document {
               `,
             }
           } />
-          <script dangerouslySetInnerHTML={
-            {
-              __html: `
-                window.twttr = (function(d, s, id) {
-                  var js, fjs = d.getElementsByTagName(s)[0],
-                    t = window.twttr || {};
-                  if (d.getElementById(id)) return t;
-                  js = d.createElement(s);
-                  js.id = id;
-                  js.src = "//platform.twitter.com/widgets.js";
-                  fjs.parentNode.insertBefore(js, fjs);
-
-                  t._e = [];
-                  t.ready = function(f) {
-                    t._e.push(f);
-                  };
-
-                  return t;
-                }(document, "script", "twitter-wjs"));
-              `,
-            }
-          } />
-          {/* <script dangerouslySetInnerHTML={
-            {
-              __html: `
-                window.fbAsyncInit = function() {
-                  FB.init({
-                    appId            : '${229428954285068}',
-                    autoLogAppEvents : true,
-                    xfbml            : true,
-                    version          : 'v2.12'
-                  });
-                };
-
-                (function(d, s, id){
-                  var js, fjs = d.getElementsByTagName(s)[0];
-                  if (d.getElementById(id)) {return;}
-                  js = d.createElement(s); js.id = id;
-                  js.src = "https://connect.facebook.net/en_US/sdk.js";
-                  fjs.parentNode.insertBefore(js, fjs);
-                }(document, 'script', 'facebook-jssdk'));
-              `,
-            }
-          } /> */}
         </Head>
 
         <body>
