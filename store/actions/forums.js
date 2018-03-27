cdf47bd82f3158c041bdd6ed4ab6e6cbf5021dd4
Cameron Welter <camjwelt@gmail.com>
2018-03-27 21:09:06 +0000

    refactor(Store): refactor to adapt to updated actionCreators

--- store/actions/forums.js
+++ store/actions/forums.js
@@ -1,12 +1,12 @@
 // Component imports
 import actionTypes from '../actionTypes'
-import createAction, { createTimeoutAction } from '../actionCreators'
+import { createApiAction, createTimeoutAction } from '../actionCreators'
 
 
 
 
 
-export const createForumThread = thread => createAction({
+export const createForumThread = thread => createApiAction({
   actionType: actionTypes.CREATE_FORUM_THREAD,
   url: '/api/threads',
   method: 'post',
@@ -19,7 +19,7 @@ export const createForumThread = thread => createAction({
   onError: 'Thread post failed.\nPlease try again in a few moments.',
 })
 
-export const createThreadComment = (threadId, comment) => createAction({
+export const createThreadComment = (threadId, comment) => createApiAction({
   actionType: actionTypes.CREATE_THREAD_COMMENT,
   url: `/api/threads/${threadId}/comments`,
   method: 'post',
@@ -35,32 +35,34 @@ export const createThreadComment = (threadId, comment) => createAction({
 })
 
 // WHEN ENDPOINT IS READY:
-// Turn this to a normal createAction, remove timeout, and uncomment remaining params.
+// Turn this to a normal createApiAction, remove timeout, and uncomment remaining params.
 export const deleteForumThread = () => createTimeoutAction({
   actionType: actionTypes.DELETE_FORUM_THREAD,
   // url: `/api/threads/${id}`,
   // method: 'delete',
   timeout: 750,
-  onError: 'Thread deletion failed.\nPlease try again in a few moments, or attempt to refresh the page.',
+  fail: true,
+  onError: 'Thread delection failed.\nThis feature is still being built!',
 })
 
 // WHEN ENDPOINT IS READY:
-// Turn this to a normal createAction, remove timeout, and uncomment remaining params.
+// Turn this to a normal createApiAction, remove timeout, and uncomment remaining params.
 export const deleteThreadComment = () => createTimeoutAction({
   actionType: actionTypes.DELETE_THREAD_COMMENT,
   // url: `/api/threads/${threadId}/comments/${commentId}`,
   // method: 'delete',
   timeout: 750,
-  onError: 'Comment deletion failed.\nPlease try again in a few moments, or attempt to refresh the page.',
+  fail: true,
+  onError: 'Thread delection failed.\nThis feature is still being built!',
 })
 
-export const getForumThread = threadId => createAction({
+export const getForumThread = threadId => createApiAction({
   actionType: actionTypes.GET_FORUM_THREAD,
   url: `/api/threads/${threadId}`,
   onError: 'Failed to load thread.\nPlease refresh the page in a few moments and try again.',
 })
 
-export const getForumThreads = page => createAction({
+export const getForumThreads = page => createApiAction({
   actionType: actionTypes.GET_FORUM_THREADS,
   url: '/api/threads',
   params: {
@@ -69,12 +71,11 @@ export const getForumThreads = page => createAction({
   onError: 'Failed to load the forums.\nPlease refresh the page in a few moments and try again.',
 })
 
-export const getThreadComments = (threadId, page) => createAction({
+export const getThreadComments = (threadId, page) => createApiAction({
   actionType: actionTypes.GET_THREAD_COMMENTS,
   url: `/api/threads/${threadId}/comments`,
   params: {
     page,
-    limit: 5,
   },
   onError: 'Failed to load thread comments.\nPlease refresh the page in a few moments and try again.',
 })
