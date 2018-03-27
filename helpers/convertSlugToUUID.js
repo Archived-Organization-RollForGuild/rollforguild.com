9c39b307346e5fb847eb0f599853af8862f93d6c
Trezy <tre@trezy.com>
2018-03-27 12:48:52 -0500

    chore(*): Fix a typo

--- helpers/convertSlugToUUID.js
+++ helpers/convertSlugToUUID.js
@@ -4,6 +4,6 @@ import uuidv5 from 'uuid/v5'
 
 
 
-export default function convertSlugtoUUID (slug, entityType) {
+export default function convertSlugToUUID (slug, entityType) {
   return uuidv5(slug, uuidv5(`https://rollforguild.com/${entityType}/`, uuidv5.URL))
 }
