b3e102c58153ca8017de790dac109a607e7fcf00
Cameron Welter <camjwelt@gmail.com>
2018-04-04 19:39:03 +0000

    feat(store): add session verification action.
    also, add various state handling changs to support verification

--- store/actions/authentication.js
+++ store/actions/authentication.js
@@ -61,9 +61,6 @@ export const login = (email, password) => createApiAction({
     Cookies.set('userId', data.data.attributes.user_id, { expires: 365 })
   },
   onError: (error) => {
-    Cookies.remove('accessToken')
-    Cookies.remove('userId')
-
     if (error.response.status === 401) {
       return createAlertObject('Make sure your email and password are correct!', 'error', 'Login Failure!', null, true)
     }
@@ -76,10 +73,16 @@ export const login = (email, password) => createApiAction({
 
 
 
-export const logout = () => async dispatch => {
+export const logout = fromVerification => async dispatch => {
   Cookies.remove('accessToken')
   Cookies.remove('userId')
-  dispatch({ type: actionTypes.LOGOUT })
+  dispatch({
+    payload: {
+      origin: fromVerification ? 'verify' : 'user',
+    },
+    status: 'success',
+    type: actionTypes.LOGOUT,
+  })
 }
 
 
@@ -144,4 +147,13 @@ export const resetPassword = (password, token) => createApiAction({
     },
   },
   onError: 'Password reset failed.\nMake sure your reset token is correct.',
+})
+
+
+
+
+
+export const verifySession = token => createApiAction({
+  actionType: actionTypes.VERIFY_SESSION,
+  url: `/api/sessions/${token}`,
 })
