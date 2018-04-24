9c6b1b743cdab8b1d0bc7e23b38005c6abac2817
Cameron " UncleClapton <camjwelt@gmail.com>
2018-04-24 12:01:03 -0400

    refactor(GameInput): render version numbers to differentiate options.

--- components/GameInput.js
+++ components/GameInput.js
@@ -60,7 +60,14 @@ class GameInput extends Component {
       return value
     }
 
-    return value.attributes.name
+    let versionString = value.attributes.version
+
+    // If the version is just a number, append an e (for edition).
+    if (!Number.isNaN(+versionString)) {
+      versionString = `${versionString}e`
+    }
+
+    return `${value.attributes.name} ${versionString}`
   }
 
 
