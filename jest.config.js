795b012890df6065ed0b20b038ecad8b3a0813b5
Trezy <tre@trezy.com>
2018-05-06 06:57:08 -0500

    fix: Add coverage reporting
    WEB-110

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-  testEnvironment: 'enzyme',
+  collectCoverageFrom: [
+    '{components,helpers,pages,services,store}/**/*.js',
+    'server/**/*.js',
+    '{routes,store}.js',
+  ],
   moduleNameMapper: {
     // '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.scss$': '<rootDir>/__mocks__/style.mock.js',
   },
+  testEnvironment: 'enzyme',
   testPathIgnorePatterns: [
     '<rootDir>/__tests__/components/Avatar.test.js',
     '<rootDir>/__tests__/_.*',
