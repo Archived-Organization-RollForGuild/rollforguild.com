7dff95aca65ca0eaa60695655eb41550caf11f75
Cameron Welter <camjwelt@gmail.com>
2018-07-06 14:48:39 +0000

    feat(PageNavigation): add generated page navigation bar

--- components/PageNavigation.js
+++ components/PageNavigation.js
@@ -1,6 +1,32 @@
-const PageNavigation = ({ children }) => (
+import PropTypes from 'prop-types'
+
+import Link from './Link'
+
+
+
+
+
+const PageNavigation = props => (
   <nav className="page">
-    {children}
+    <ul>
+      {props.pages.map(page => {
+        const {
+          key,
+          title,
+          ...linkProps
+        } = page
+
+        const renderedTitle = typeof title === 'function' ? title() : title
+
+        return (
+          <li key={key} className={props.activePage === key ? 'active' : ''}>
+            <Link {...linkProps}>
+              {renderedTitle}
+            </Link>
+          </li>
+        )
+      })}
+    </ul>
   </nav>
 )
 
@@ -10,7 +36,20 @@ const PageNavigation = ({ children }) => (
 
 PageNavigation.defaultProps = {}
 
-PageNavigation.propTypes = {}
+PageNavigation.propTypes = {
+  activePage: PropTypes.string.isRequired,
+  pages: PropTypes.arrayOf(PropTypes.shape({
+    category: PropTypes.string.isRequired,
+    key: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    params: PropTypes.object,
+    route: PropTypes.string.isRequired,
+    title: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.func,
+    ]).isRequired,
+  })).isRequired,
+}
 
 
 
